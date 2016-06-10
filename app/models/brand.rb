f96ca87973e336ce05f7c2a63cc75b9cb2b6488b
Helen Erikson <helen.erikson@users.noreply.github.com>
2016-06-10 19:40:51 +0200

    ratings for brands

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,9 +3,10 @@ class Brand < ActiveRecord::Base
   validates :user, :presence => true
 
   belongs_to :user
+  has_many :ratings
 
   has_many :comments
-  has_many :ratings
+
   has_many :raters, :through => :ratings, :source => :user
 
 end
